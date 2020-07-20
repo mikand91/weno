d015434e8a3a983a1f8289dc86304871757d68ab
Mikolaj Andrzejewski <mikolaj.andrzejewski@ormdigital.com>
2020-07-21 00:09:01 +0200

    quick fix for hidden sections

--- js/animations/js/sections.js
+++ js/animations/js/sections.js
@@ -7,20 +7,29 @@
     /**
      * sections fade in
      */
-    $(window).scroll(function() {
+    const showSections = function(){
       var scrollTop = $(window).scrollTop();
 
       $('section').each(function(){
         if(!$(this).hasClass('active')){
           let elementOffset = $(this).offset().top;
           let distance = (elementOffset - scrollTop);
-          if(distance < ($( window ).height() - 350)){
+          if(distance < ($( window ).height() - 250)){
             $(this).addClass('active');
           }
         }
 
       });
+    };
+
+    /**
+     * sections fade in
+     */
+    $(window).scroll(function() {
+      showSections();
     });
+
+    showSections();
   });
 
 })(jQuery);
